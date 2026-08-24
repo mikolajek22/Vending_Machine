f34b4a1afc3056d13dd30490e828fda28f3d7618
mikolajek22 <mikopietrynko@gmail.com>
2026-08-24 19:54:16 +0200

    feature: Succeess propability added - simulation of the offline state

--- tests/include/tests/fakeTransport.hpp
+++ tests/include/tests/fakeTransport.hpp
@@ -2,7 +2,10 @@
 
 #include <core/iTransport.hpp>
 #include <vector>
-
+#include <atomic>
+#include <random>
+#include <vector>
+#include <iostream>
 
 namespace vending {
 
@@ -10,18 +13,33 @@ class FakeTransport : public ITransport
 {
     public:
         bool send(const Transaction &t) override {
-            if (_forcedResult)
+            float res = _rngDist(_rng);
+            std::cout << "Proba. result is: " << res << std::endl;
+            bool succeedeed = _forcedResult && res > _failureProbability;
+            _online = succeedeed;
+            if (succeedeed)
                 _sentUuids.push_back(t.uuid);
 
-            return _forcedResult;
+            return succeedeed;
         }
 
         /**
          * @brief Force result of send() - only for test
          */
         void forceResult(bool result) { _forcedResult = result; }
+
+        
+        bool isOnline() const { return _online; }
+        void setFailureProbability(double probability) { _failureProbability = probability; }
+
         std::vector<std::string> sentUuids() { return _sentUuids; }
+
     private:
+        std::atomic<bool> _online{true};
+        double _failureProbability = 0.0;
+        std::mt19937 _rng{std::random_device{}()};
+        std::uniform_real_distribution<double> _rngDist{0.0, 1.0};
+
         bool _forcedResult = true;
         /* Successfully sent Transactions (uuid) */
         std::vector<std::string> _sentUuids;
